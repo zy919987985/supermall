fb71186a8e77281df4e7b3ddc1fe748a6b7e5961
张阳阳 <zhangyangyang@vchangyi.com>
2021-08-05 16:26:27 +0800

    swiper组件

---
+++ src/views/home/childCompoents/homeSwiper.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <swiper>
+      <swiper-item
+        v-for="(item,index) in swiperList"
+        :key="index"
+      >
+        <a :href="item.link">
+          <img :src="item.iamge" alt="">
+        </a>
+      </swiper-item>
+    </swiper>
+  </div>
+</template>
+
+<script>
+import { Swiper, SwiperItem } from 'components/common/swiper'
+export default {
+  name: 'homeSwiper',
+  props: {
+    swiperList: {
+      type: Array,
+      default:() => []
+    }
+  },
+  components: {
+    Swiper,
+    SwiperItem
+  }
+}
+</script>
+
+<style>
+
+</style>
